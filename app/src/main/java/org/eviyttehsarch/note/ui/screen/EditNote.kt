4a75cbe9f57b973799a0036fb3d886f1449b924e
eviarch <eviarch666@gmail.com>
2024-03-01 21:59:13 +0800

    Optimize app icon.Modify noteEditScreen

--- app/src/main/java/org/eviyttehsarch/note/ui/screen/EditNote.kt
+++ app/src/main/java/org/eviyttehsarch/note/ui/screen/EditNote.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -22,6 +23,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import org.eviyttehsarch.note.data.NoteEntity
@@ -47,6 +49,7 @@ fun EditNote(
         ) {
             TextField(
                 modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
                     .fillMaxWidth(),
                 value = title,
                 textStyle = MaterialTheme.typography.headlineSmall,
@@ -72,6 +75,7 @@ fun EditNote(
             Spacer(modifier = Modifier.height(8.dp))
             TextField(
                 modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
                     .fillMaxWidth(),
                 value = content,
                 onValueChange = {
@@ -79,7 +83,7 @@ fun EditNote(
                     modifiedDate = System.currentTimeMillis()
                     onDone(NoteEntity(note.id, title, content, modifiedDate))
                 },
-                placeholder = { Text(text = "Title") },
+                placeholder = { Text(text = "Content") },
                 keyboardActions = KeyboardActions(
                     onDone = {
                         onDone(NoteEntity(note.id, title, content, modifiedDate))
