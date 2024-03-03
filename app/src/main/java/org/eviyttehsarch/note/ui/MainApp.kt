ce473b2fdb63b24880bb585916cdb69f25f761c4
eviarch <eviarch666@gmail.com>
2024-03-03 11:07:53 +0800

    Delete unnecessary values.

--- app/src/main/java/org/eviyttehsarch/note/ui/MainApp.kt
+++ app/src/main/java/org/eviyttehsarch/note/ui/MainApp.kt
@@ -14,10 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.unit.toSize
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -37,12 +34,8 @@ fun MainApp(
     mainViewModel: MainViewModel,
     settingsViewModel: SettingsViewModel
 ) {
-    val surfaceSize = remember { mutableStateOf(Size.Zero) }
     Surface(
         modifier = Modifier
-            .onGloballyPositioned { coordinates ->
-                surfaceSize.value = coordinates.size.toSize()
-            }
             .fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
