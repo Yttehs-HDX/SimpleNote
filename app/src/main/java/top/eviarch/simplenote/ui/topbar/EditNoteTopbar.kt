8da3488bf6278b4b76d0146e720dc12982314c2a
eviarch <eviarch666@gmail.com>
2024-03-05 23:46:55 +0800

    Reset scrollBehavior

--- app/src/main/java/top/eviarch/simplenote/ui/topbar/EditNoteTopbar.kt
+++ app/src/main/java/top/eviarch/simplenote/ui/topbar/EditNoteTopbar.kt
@@ -15,8 +15,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
@@ -61,7 +61,7 @@ fun EditNoteTopBar(
     ) {
         var showDialog by remember { mutableStateOf(false) }
         val interactionSource = remember { MutableInteractionSource() }
-        MediumTopAppBar(
+        TopAppBar(
             scrollBehavior = scrollBehavior,
             colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = MaterialTheme.colorScheme.primaryContainer,
