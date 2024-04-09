013e9eac72e9f4dbd9cc7a99e0b47acd20f969c2
Shetty Yttehs <yttehs@ROG-Strix-G16.localdomain>
2024-04-09 20:47:31 +0800

    Update version number.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "top.eviarch.simplenote"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
